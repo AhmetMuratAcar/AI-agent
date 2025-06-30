fe35d3ae0b2602cadb71a31f656b5fbf89d26771
AhmetMuratAcar <muratacar.dev@gmail.com>
2025-06-30 08:42:27 -0700

    add: get_file_content and its tests

---
+++ functions/get_file_content.py
@@ -0,0 +1,31 @@
+import os
+
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    try:
+        full_path = os.path.join(working_directory, file_path)
+
+        # Check validity of file path
+        if not os.path.isfile(full_path):
+            return f'Error: "{full_path}" is not a file'
+
+        # Is file_path in working_directory
+        abs_working_path = os.path.abspath(working_directory)
+        abs_full_path = os.path.abspath(full_path)
+        secure_check = abs_full_path.startswith(abs_working_path)
+
+        if not secure_check:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        # Read and write file content
+        MAX_CHARS = 10000
+        with open(full_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+
+        if len(file_content_string) == 10000:
+            file_content_string += f'\n[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+        return file_content_string
+
+    except Exception as e:
+        return f"Error: {str(e)}"
--- tests.py
+++ tests.py
@@ -1,13 +1,15 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
-def run_tests():
+def test_get_files_info():
+    print("=== Testing get_files_info ===")
     test_cases = [
         ("calculator", "."),       # Expected: valid result
         ("calculator", "pkg"),     # Expected: valid result
         ("calculator", "/bin"),    # Expected: error string
         ("calculator", "../"),     # Expected: error string
-        ("calculator", None)
+        ("calculator", None)       # Expected: valid result
     ]
 
     for i, (working_dir, directory) in enumerate(test_cases, 1):
@@ -16,5 +18,21 @@ def run_tests():
         print(result, "\n")
 
 
+def test_get_file_content():
+    print("=== Testing get_file_content ===")
+    test_cases = [
+        ("calculator", "main.py"),               # Expected: valid result
+        ("calculator", "pkg/calculator.py"),     # Expected: valid result
+        ("calculator", "/bin/cat"),              # Expected: error or permission issue
+        # ("calculator", "lorem.txt")              # Expected: valid truncated result
+    ]
+
+    for i, (working_dir, file_path) in enumerate(test_cases, 1):
+        print(f"Test {i}: get_file_content({working_dir!r}, {file_path!r})")
+        result = get_file_content(working_dir, file_path)
+        print(result, "\n")
+
+
 if __name__ == "__main__":
-    run_tests()
+    test_get_files_info()
+    test_get_file_content()
